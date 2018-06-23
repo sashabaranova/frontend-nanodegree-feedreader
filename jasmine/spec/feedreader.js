5dae1ef30b2f4a823f8a64f40d7633c9c8c9baaa
hija_de_puta <bad.communists@gmail.com>
2018-06-23 19:06:22 +0800

    last test suite fixed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -71,7 +71,6 @@ $(function() {
          */
         it('is hidden', () => {
 
-            // NB: in the most recent version of Jasmine we would use 'toHaveClass' method
             expect(body).toHaveClass('menu-hidden');
         });
 
@@ -99,6 +98,7 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+
         beforeEach(done => {
             loadFeed(0, () => done());
         });
@@ -112,6 +112,7 @@ $(function() {
         });
     });
 
+
     describe('New Feed Selection', () => {
     /* TODO: Write a new test suite named "New Feed Selection" */
 
@@ -119,31 +120,96 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
         const feed = $('.feed')[0];
-        // new and old content variables
-       let currFeedContent, newFeedContent;
+
+        // OPTION 1 - we define a function that gets all feeds content concurrently, using JS Promises
+        function getFeedsConcurrently(cb) {
+                
+            // this array will contain promises
+            let promises = [];
+
+            // looping over the number of feeds, we create a promise
+            // that calls loadFeed function for each feed
+            for (let i = 0; i < allFeeds.length; i++) {
+                let promise = new Promise((resolve, reject) => {
+                    loadFeed(i, () => resolve(feed.innerHTML));
+                    // we pass HTML content of the feed container in the resolve function
+                });
+                // all promises are added to the array
+                promises.push(promise);
+            }
+
+            // we use Promise.all method to create a new promise that will wait until all the promises have resolved
+            // contents is the array that stores all HTML contents from the previous promises; it is passed into a callback function
+            // and used to create a new set stored in the contentSet variable
+            Promise.all(promises).then(cb);
+        }
+
+        // OPTION 2 - we define a function that gets feeds content sequentially 
+
+        function getFeedsSequentionally(cb) {
+            // internal variable storing feed contents
+            let contents = [];
+
+            // function f is used for calling loadFeed recursively
+            // internal state of recursion is controlled with 'i' that is passed to the function as a parameter
+            function f(i) {
+                contents.push(feed.innerHTML);
+
+                // the recursion is stopped when we run out of feeds, and cb is called
+                if (i >= allFeeds.length - 1) {
+                    cb(contents);
+                    return;
+                }
+
+                i++;
+
+                loadFeed(i, () => f(i));
+            }
+
+            loadFeed(0, () => f(0)); 
+
+        }
+
+        // array that will store all feeds contents
+        let contents = [];
 
         beforeEach(done => {
 
-            //default feed index;
-            const i = 0;
+            const callback = cnts => {
+                contents = cnts;
+                // back to default feed
+                loadFeed(0);
+                //done is called when all the promises have resolved / recursion has finished
+                done();
+            }
 
-            // to check different feed options j is a random feed index ranging from 1 to 3)
-            const j = 1 + Math.floor(Math.random() * 3);
+            // OPTION 1
 
-            loadFeed(i, () => {
-                currFeedContent = feed.innerHTML;
-                loadFeed(j, () => {
-                    newFeedContent = feed.innerHTML;
-                    done();
-                });
-            });
-        });
+            getFeedsConcurrently(callback);
+
+            /* OPTION 2
+             * Both functions give the same result and return contents array, but the sequential function takes longer
+             * to get all feeds, so we pass a timeout into beforeEach
+             */
+
+
+            // getFeedsSequentionally(callback);
+
+
+        }, 60000);
 
         it('Feed content changes', done => {
+            // OPTION 1
+
+            const contentSet = new Set(contents);
+
             // as a set only stores unique values, we made sure that in case feed content did not change, it would be overwritten
             // if the size of the set equals allFeeds.length, it means that all feeds are different and the content does change:)
-            expect(currFeedContent).not.toEqual(newFeedContent);
+            expect(contentSet.size).toEqual(allFeeds.length);
+
+            // OPTION 2
             done();
         });
 
